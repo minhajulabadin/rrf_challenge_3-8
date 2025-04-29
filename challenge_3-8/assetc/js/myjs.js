b2428e7ae7671b2378825e2f75cb32eadad207c8
Minhajul Abadin <92793488+minhajulabadin@users.noreply.github.com>
2025-04-29 20:55:23 +0600

    Add files via upload

---
+++ challenge_3-8/assetc/js/myjs.js
@@ -0,0 +1,14 @@
+const btnon = document.getElementById ('bn-on');
+const btnoff = document.getElementById ('bn-off');
+
+
+btnon.addEventListener('click', function (){
+  const tt = document.getElementById('hh')
+    tt.style.display = 'block'
+})
+
+
+btnoff.addEventListener('click', function (){
+  const tt = document.getElementById('hh')
+    tt.style.display = 'none'
+})
